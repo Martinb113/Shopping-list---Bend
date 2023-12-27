6dd33057da8dfe677f26e8403ceec48d6f5de05e
Martinb113 <martinb113@gmail.com>
2023-12-27 23:39:45 +0100

    Routes for Items has been modified

--- routes/items.js
+++ routes/items.js
@@ -2,18 +2,25 @@ const express = require('express');
 const Item = require('../models/item'); // Replace with your actual Item model path
 const router = express.Router();
 
-// Add Item to a List
+
+//Add Item to Shopping List
 router.post('/', async (req, res) => {
+    // Logic to add an item to a shopping list
+    const newItem = new Item({
+        name: req.body.name,
+        quantity: req.body.quantity,
+        listId: req.body.listId,
+        //owner: req.body.owner
+    });
     try {
-        const item = new Item({ 
-            name: req.body.name,
-            quantity: req.body.quantity,
-            listId: req.body.listId 
+        await newItem.save();
+        res.status(201).json({
+            itemId: newItem._id,
+            success: true,
+            message: 'Item added to shopping list successfully'
         });
-        await item.save();
-        res.status(201).send(item);
     } catch (error) {
-        res.status(500).send('Error in adding item.');
+        res.status(500).json({ success: false, message: error.message });
     }
 });
 
@@ -26,5 +33,70 @@ router.get('/list/:listId', async (req, res) => {
         res.status(500).send('Error in retrieving items.');
     }
 });
+
+// Update an Item
+router.put('/update/:itemId', async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        const { name, quantity, status } = req.body;
+
+        // Find and update the item
+        const item = await Item.findById(itemId);
+        if (!item) {
+            return res.status(404).json({ success: false, message: 'Item not found' });
+        }
+
+        // Update item details
+        if (name) item.name = name;
+        if (quantity) item.quantity = quantity;
+        if (status) item.status = status;
+
+        await item.save();
+        res.status(200).json({ success: true, message: 'Item updated successfully', item });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+});
+
+// Delete an Item
+router.delete('/delete/:itemId', async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+
+        // Find and delete the item
+        const item = await Item.findById(itemId);
+        if (!item) {
+            return res.status(404).json({ success: false, message: 'Item not found' });
+        }
+
+        await item.remove();
+        res.status(200).json({ success: true, message: 'Item deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+});
+
+// Mark Item as Completed
+router.put('/complete/:itemId', async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+
+        // Find the item and update its status to 'Completed'
+        const item = await Item.findById(itemId);
+        if (!item) {
+            return res.status(404).json({ success: false, message: 'Item not found' });
+        }
+
+        item.completed = true; // Assuming there's a 'completed' field in the Item model
+        await item.save();
+
+        res.status(200).json({ success: true, message: 'Item marked as completed', item });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+});
+
+
+
 
 module.exports = router;
